01a7d2d57ac07008d33b686df790843cc37c2d73
sravan4205 <sravan.amishetti@gmail.com>
2020-01-20 16:45:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent {
-  title = 'viewchiled';
+  @ViewChild('maincontent') mainContent: any;
+  @ViewChild('subcontent') subContent: any;
+
+  name = 'Angular 5';
+
+  public changeMainContent() {
+    console.log('changeMainContent');
+    this.mainContent.nativeElement.setAttribute('style', 'color:red');
+  }
+
+  public changeSubContent() {
+    console.log('changeSubContent');
+    this.subContent.nativeElement.setAttribute('style', 'color:green');
+  }
 }
